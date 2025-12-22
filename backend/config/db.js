b47688cf82a8e85c01cb5a57f896cc0e3c51f883
lazarjava <lazartableau@gmail.com>
2025-12-23 04:57:53 +0530

    Refactor DB connection to use MONGO_URI
    Updated MongoDB connection string to use environment variable.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://lazarjava352_db_user:Common%40123@cluster0.w3bug92.mongodb.net/food-app');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1); // app stop aagum if DB fail
+    console.error("MongoDB connection failed:", error);
+    throw error; // IMPORTANT
   }
 };
