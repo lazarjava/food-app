491555c731100dc031d1ad256f646dbbb2d821ee
lazarjava <lazartableau@gmail.com>
2025-12-23 03:49:39 +0530

    Improve DB connection logic with error handling
    Refactor database connection to use environment variable and add error handling.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
-export const connectDB =async () => {
-    await mongoose.connect('mongodb+srv://lazarjava352_db_user:Common%40123@cluster0.w3bug92.mongodb.net/food-app').then(()=>console.log("DB Connected"));
-}
-
-
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); // app stop aagum if DB fail
+  }
+};
