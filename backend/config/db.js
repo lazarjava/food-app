447d8f49446d25823d3a02f02842c31678f84136
lazarjava <lazartableau@gmail.com>
2025-12-23 04:32:49 +0530

    Change MongoDB connection string and error handling
    Updated MongoDB connection URI and modified error handling.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect('mongodb+srv://lazarjava352_db_user:Common%40123@cluster0.w3bug92.mongodb.net/food-app');
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
-    // ❌ DO NOT exit process on Render
+    process.exit(1); // app stop aagum if DB fail
   }
 };
