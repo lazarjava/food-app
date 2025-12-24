f4fde593643eea05a6ea4b8343d86b66fc293643
lazarjava <lazartableau@gmail.com>
2025-12-24 12:19:56 +0530

    Fix import path for StoreContext in Navbar component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 import {assets} from '../../assets/assets'
 import joyful from '../../assets/joyful.gif';
 import {Link, useNavigate} from 'react-router-dom'
-import { StoreContext } from '../../Context/StoreContext'
+import { StoreContext } from '../../context/StoreContext'
 
 
 const Navbar = ({setShowLogin}) => {
