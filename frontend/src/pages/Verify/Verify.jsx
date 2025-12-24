637d8bc7b1f5429cb77613c8ed6e8048442abcf0
lazarjava <lazartableau@gmail.com>
2025-12-24 12:29:55 +0530

    Fix import path for StoreContext

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useContext } from 'react';
 import "./Verify.css";
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { StoreContext } from '../../Context/StoreContext';
+import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios';
 
 const Verify = () => {
